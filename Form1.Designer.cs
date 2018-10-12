d2ed56eaf1d8a7e72598696906c930e5218fb8ff
Michael.Doyle@statravel.com <nAturfARPulA1>
2018-10-12 17:38:51 +0100

    V1

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace SumOrSequentialNumbers
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.AllowColumnReorder = true;
+			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+			this.listView1.FullRowSelect = true;
+			this.listView1.GridLines = true;
+			this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+			this.listView1.Location = new System.Drawing.Point(12, 78);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(1496, 556);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Number";
+			this.columnHeader1.Width = 103;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Number of ways";
+			this.columnHeader2.Width = 122;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Divisors";
+			this.columnHeader3.Width = 797;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(12, 29);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown1.TabIndex = 1;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+			this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Location = new System.Drawing.Point(161, 29);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown2.TabIndex = 2;
+			this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 17);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Start";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(158, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(33, 17);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "End";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.numericUpDown2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.numericUpDown1);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(443, 60);
+			this.panel1.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(317, 28);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(89, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Go";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "Ways";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(1359, 41);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(149, 31);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Copy results";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1520, 646);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.listView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumOrSequentialNumbers
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			listView1.Items.Clear();
+			for (var number = Convert.ToInt32(numericUpDown1.Value); number <= Convert.ToInt32(numericUpDown2.Value); number++)
+			{
+
+
+				listView1.Items.Add(new Number(number).AsListViewItem());
+			}
+		}
+
+		public class Number
+		{
+			private string ListToString(IEnumerable<int> list)
+			{
+				return string.Join(",", list.Select(x => x.ToString()));
+
+			}
+
+			public ListViewItem AsListViewItem()
+			{
+				return new ListViewItem(new[] { Value.ToString(), Ways.Count.ToString(), ListToString(Divisors), WaysToString() });
+			}
+
+			private string WaysToString()
+			{
+				return string.Join(";",
+					Ways.Select(way => "{" + way.Count + ":= " + ListToString(way) + "}")
+				);
+			}
+
+			public Number(int number)
+			{
+				this.Value = number;
+				FindWays();
+			}
+
+			public int Value { get; set; }
+
+			public List<int> Divisors { get; set; } = new List<int>();
+			public List<List<int>> Ways { get; set; } = new List<List<int>>();
+
+			private void FindWays()
+			{
+
+				for (int ii = 2; ii <= Value / 2 + 1; ii++)
+				{
+					if (Value % ii == 0) Divisors.Add(ii);
+
+					var fraction = Value / ii;
+					var smallestStart = Math.Max(1, fraction - ii - 1);
+					for (var start = smallestStart; start <= ii; start++)
+					{
+						if (Ways.Any(existingWay => existingWay.First() == start)) continue;
+						var way = new List<int>();
+						var sum = 0;
+						var num = start;
+						while (sum < Value)
+						{
+							sum += num;
+							way.Add(num);
+							num++;
+						}
+
+						if (sum == Value)
+						{
+							Ways.Add(way);
+						}
+					}
+				}
+			}
+		}
+
+
+
+
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			var sb = new StringBuilder();
+			foreach (ListViewItem item in listView1.Items)
+			{
+				foreach (ListViewItem.ListViewSubItem value in item.SubItems)
+				{
+					sb.Append(value.Text);
+					sb.Append("\t");
+				}
+				sb.AppendLine();
+			}
+
+			Clipboard.SetText(sb.ToString(), TextDataFormat.UnicodeText);
+		}
+	}
+}
